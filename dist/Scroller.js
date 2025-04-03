a5b0f5fcb69d8f53b0e7037f6a1b0ef0677cd730
Deeter Cesler <me@deetercesler.com>
2025-04-02 23:02:35 -0400

    make X invisible on load

--- dist/Scroller.js
+++ dist/Scroller.js
@@ -5,13 +5,16 @@ export default function Scroller(props) {
   // TODO: add caching so it's not constantly recalculating body window
   // I can't just declare it up here because on initial
   // load it sees the DOM has a height of zero.
+
   $(window).on('scroll', function () {
     const bodyEl = $(".body-content");
-    const bottomOfContent = bodyEl.get(0).getBoundingClientRect().height + bodyEl.offset().top; // the percentage of catch-up - the accelerator: 
+    const bottomOfContent = bodyEl.get(0).getBoundingClientRect().height + bodyEl.offset().top;
 
+    // the percentage of catch-up - the accelerator: 
     const scrollPercentAccelerator = 1 + bodyEl.offset().top / bottomOfContent;
     const scrollDistance = $(window).scrollTop() + $(window).height() - bodyEl.offset().top;
     const scrollPercentage = Math.min(scrollDistance * scrollPercentAccelerator / bottomOfContent * 100, 100);
+
     /*  
         I hate this but I don't understand dasharray-offset enough to do better.
          0% read is 200% stroke offset.
@@ -20,22 +23,23 @@ export default function Scroller(props) {
     */
 
     const magicMultiplier = 2; // because e.g. 50% read * 2.4 => 120%, making the stroke offset halfway
-
     const magicNumber = 200;
     const circleFill = 40;
     const offSet = Math.max(-40, magicNumber - scrollPercentage * magicMultiplier);
     const checkedOffset = offSet >= magicNumber ? magicNumber : offSet + circleFill;
     $("#circle-lol").attr("stroke-dashoffset", `${checkedOffset}%`);
-    if (offSet < magicNumber - circleFill) $(".white-wrapper").removeClass('hidden').addClass('pop-in-initial');
+    if (offSet < magicNumber - circleFill) {
+      $(".white-wrapper").removeClass('hidden').addClass('pop-in-initial');
+      $(".x-mark").removeClass('hidden').addClass('pop-in-initial');
+    }
     const circleProgressionPercentage = parseInt($("#circle-lol").attr("stroke-dashoffset"));
-
     if (circleProgressionPercentage <= circleFill && $(".checkmark").hasClass('hidden')) {
       $(".checkmark").removeClass('hidden').addClass('pop-in');
       $(".x-mark").addClass('hidden').removeClass('pop-in');
       $("#circle-lol").addClass('grey-out');
-    } // this is a separate if-check to prevent adding the class repeatedly
+    }
 
-
+    // this is a separate if-check to prevent adding the class repeatedly
     if (circleProgressionPercentage > circleFill && $(".checkmark").hasClass('pop-in')) {
       $(".checkmark").addClass('hidden').removeClass('pop-in');
       $(".x-mark").removeClass('hidden').addClass('pop-in');
@@ -104,7 +108,7 @@ export default function Scroller(props) {
     strokeWidth: "1",
     d: "m10.5 39.76 17.42 17.44 33.58-33.89-8.013-8.013-25.71 25.71-9.26-9.26z"
   })))), /*#__PURE__*/React.createElement("div", {
-    className: "x-mark"
+    className: "x-mark hidden"
   }, /*#__PURE__*/React.createElement("svg", {
     id: "x-icon",
     fill: "#000",
@@ -114,7 +118,7 @@ export default function Scroller(props) {
     xmlns: "http://www.w3.org/2000/svg"
   }, /*#__PURE__*/React.createElement("path", {
     d: "M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55  c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55  c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505  c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55  l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719  c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"
-  }), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null), /*#__PURE__*/React.createElement("g", null))), /*#__PURE__*/React.createElement("div", {
+  }))), /*#__PURE__*/React.createElement("div", {
     className: "white-wrapper hidden"
   }, /*#__PURE__*/React.createElement("svg", {
     viewBox: "0 0 100 100",
